a0e0c384330d03769aa9f3917431d13c9efc576a
Liuyushuai <3058756683@qq.com>
2020-08-05 23:42:56 +0800

    number1

---
+++ src/views/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="login">
+    我是登录页
+    <el-button>默认按钮</el-button>
+  </div>
+</template>
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
